63697e76c6e2439c2d709877fa44fbb0dfefd6a0
kangpeng <tony_kanper@hotmail.com>
2016-12-20 11:10:40 +0800

    给控件增加停止方法

--- js/plugin.js
+++ js/plugin.js
@@ -104,6 +104,7 @@
         },
         pauseTime: function () {
             var $this = $(this);
+            var $pointer = $(this).find("#pointer");
             var $play = $this.find('#play');
             $play.find("i").removeClass('horizontal-pause-div horizontal-play-div').addClass('horizontal-play-div');
             $play.find("span").text("播放");
